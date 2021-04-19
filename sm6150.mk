dafdb65ede8350b7361826153fc46e59fadc8477
enrico-mo <enrico.caporali.mo@gmail.com>
2021-04-19 14:19:11 +0200

    sm6150-common Device Tree rebase from github.com/Haky86/aandroid_device_samsung_sm6150-common.git

---
+++ sm6150.mk
@@ -0,0 +1,87 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/samsung/sm6150-common
+
+PRODUCT_TARGET_VNDK_VERSION := 29
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Proprietary blobs
+$(call inherit-product-if-exists, vendor/samsung/sm6150-common/sm6150-common-vendor.mk)
+
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    $(COMMON_PATH)/overlay \
+    $(COMMON_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_TARGETS += *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(COMMON_PATH)/overlay-lineage/lineage-sdk \
+    $(COMMON_PATH)/overlay-lineage/packages/apps/Snap
+
+# Audio 
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
+
+# Init
+PRODUCT_PACKAGES += \
+    init.qcom.rc
+
+# OTA Updater
+AB_OTA_UPDATER := false
+
+# NFC
+PRODUCT_PACKAGES += \
+    libnfc-nci \
+    libnfc_nci_jni \
+    NfcNci \
+    Tag \
+    com.android.nfc_extras
+
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.3-service.sm6150
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/etc/powerhint.json
+
+# Recovery
+PRODUCT_PACKAGES += \
+    fastbootd \
+    init.recovery.qcom.rc
+
+# SamsungDoze
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl.samsung-sm6150
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:system/etc/permissions/android.hardware.sensor.hifi_sensors.xml
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
+# Trust HAL
+PRODUCT_PACKAGES += \
+    lineage.trust@1.0-service
